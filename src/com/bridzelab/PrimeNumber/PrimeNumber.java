d67841775fae52f10861a9b3c1523211a8e1393f
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-07-23 16:04:26 +0530

    updated declaration of value to the user input

--- src/com/bridzelab/PrimeNumber/PrimeNumber.java
+++ src/com/bridzelab/PrimeNumber/PrimeNumber.java
@@ -1,28 +1,22 @@
-/* Q-3.) Prime Number
-		Just like the Perfect number, the Prime number is also a special type of number. When
-		the number is divided greater than 1 and divided by 1 or itself is referred to as the Prime
-		number. 0 and 1 are not counted as prime numbers. All the even numbers can be
-		divided by 2, so 2 is the only even prime numbers.
-		*/
 package com.bridzelab.PrimeNumber;
-import java.util.Scanner;   // Import Scanner
+import java.util.Scanner;   
 
 public class PrimeNumber {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);  //make scanner obj
-		        System.out.println("Enter a number: ");
-		        int num = scanner.nextInt(); //Input int
-		        int count=0;
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt(); //Input int
+        int flag=0;
+	    for (int i = 2; i <= num / 2; ++i) {
+	      // condition for nonprime number
+	      if (num % i == 0) {
+	        flag++;
+	      }
+	    }
 
-		        for(int i=2;i<num;i++){
-		            if (num%i==0){
-		                count++;
-		            }
-		        }
-		        if (count==0){
-		            System.out.println(num+ " is a prime number");
-		        }else
-		            System.out.println(num + " is not a prime number");
-		        }
-
-		}
+	    if (flag==0)
+	      System.out.println(num + " is a prime number.");
+	    else
+	      System.out.println(num + " is not a prime number.");
+	  }
+	}
